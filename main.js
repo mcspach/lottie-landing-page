7308af9f1027417e60ca3fe3bdd45af0c83fc278
Matthew Spach <matthewspach@gmail.com>
2021-03-18 07:36:33 -0700

    working with vue instance

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
+//import vue
+const app = new Vue({
+    el: "#app",
+    data: {
+        brand: 'Impact'
+    }
+})
+
 //animate buttons after interval
 //listen for button click
 //play lottie animation
 //reveal that div
 //listen for button clicks
+//need one more
